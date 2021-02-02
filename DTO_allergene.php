c5838bf20fa32db4fc2cdda14a674384ee4c14f3
beaudetamelie <70749126+beaudetamelie@users.noreply.github.com>
2021-02-02 11:21:18 +0100

    Delete DTO_allergene.php

--- DTO_allergene.php
+++
@@ -1,9 +0,0 @@
-<?php
-class DTO_allergene
-{
-	private $nom;
-function __construct($nom){
-	$this->nom =$nom;
-}
-public function getNom() {return $this ->nom;}
-public function setNom($nom) {$this->nom =$nom;}}
