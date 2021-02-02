4a8a89b8b9079d631cf8a7a2f44b0bb417537631
Amélie Beaudet <beaudetamelie1502@gmail.com>
2021-02-02 16:46:30 +0100

    commit lundi soir

--- admin.php
+++ admin.php
@@ -19,27 +19,41 @@ try
 </head>
 <body>
 <h1>Partie Administration</h1>
-	<p>Ajouter un plat</p>
+	<p><strong><i>Ajouter un plat</i></strong></p>
 	<form action="admin.php" method="post">
+		<!-- <div>
+        <label for="IdPlat" class="form"></label>
+        <input type="text" id="IdPlat" name="IdPlat" class="input">
+    </div>-->
     <div>
-        <label for="nomPlat">Nom du plat :</label>
-        <input type="text" id="nomPlat" name="nomPlat">
+        <label for="nomPlat" class="form">Nom du plat :</label>
+        <input type="text" id="nomPlat" name="nomPlat" class="input">
     </div>
+    <br/>
     <div>
-        <label for="prixPlat">prix du plat :</label>
-        <input type="text" id="prixPlat" name="prixPlat">
+        <label for="prixPlat" class="form">prix du plat :</label>
+        <input type="text" id="prixPlat" name="prixPlat"class="input">
     </div>
+    <br/>
     <div>
-        <label for="vegetarien">Plat végétarien  (0/1) :</label>
-        <input type="text" id="vegetarien" name="vegetarien">
+        <label for="vegetarien"class="form">Plat végétarien  (0/1) :</label>
+        <input type="text" id="vegetarien" name="vegetarien" class="input">
     </div>
+    
+    <br/>
     <div>
-        <label for="type">Type de plat (entree/plat/dessert) :</label>
-        <input type="text" id="type" name="type">
+        <label for="type"class="form">Type de plat (entree/plat/dessert) :</label>
+        <input type="text" id="type" name="type" class="input">
     </div>
+    <br/>
     <div>
-    	 <label for="type"></label>
-        <input type="submit" id="valider" name="valider">
+        <label for="type"class="form">ID Allergène  (Oeuf = 4/Poisson = 3/Lait = 2/Arachide = 1) :</label>
+        <input type="text" id="id_allergene" name="id_allergene" class="input">
+    </div>
+    <br/>
+    <div>
+    	 <label for="type"class="form"></label>
+        <input type="submit" id="valider" name="valider" class="input">
      </div>
 </form>
 <?php
@@ -48,19 +62,26 @@ $nomPlat = $_POST['nomPlat'];
 $prixPlat = $_POST['prixPlat'];
 $vegetarien = $_POST['vegetarien'];
 $type = $_POST['type'];
+$id_allergene = $_POST['id_allergene'];
 
 //On prépare la commande sql d'insertion
 $requete21 = "INSERT INTO plat (id_plat,nomPlat, prix, vegetarien, type) VALUES(?, ?, ?, ?, ?)";
 $req = $bdd->prepare($requete21);
 	   $req->execute([NULL, $nomPlat, $prixPlat,$vegetarien, $type]);
-}
-?>
-<p>Supprimer un plat</p>
+
+
+
+$requete24 = "INSERT INTO plat_allergene(id_plat, id_allergene) VALUES((SELECT id_plat FROM plat WHERE $nomPlat = nomPlat), ?)" ;
+$req = $bdd->prepare($requete24);
+	   $req->execute([$id_allergene]); 
+}?>
+<p><strong><i>Supprimer un plat</i></strong></p>
 <form action="admin.php" method="post">
     <div>
         <label for="nomPlatSuppr">Nom du plat à supprimer :</label>
         <input type="text" id="nomPlatSuppr" name="nomPlatSuppr">
     </div>
+    <br/>
     <div>
     	 <label for="type"></label>
         <input type="submit" id="ok" name="ok">
@@ -68,18 +89,41 @@ $req = $bdd->prepare($requete21);
     <?php
  if (isset ($_POST['ok'])){
 $nomPlatSuppr = $_POST['nomPlatSuppr'];
-$requete22 = "SELECT id_plat FROM plat WHERE nomPlat = ?";
-$req = $bdd->prepare($requete22);
-	   $req->execute(['$nomPlatSuppr']);
-	   while ($donnees = $req->fetch())
-{
-       echo $donnees['id_plat'];
-}
-/*$requete21 = "DELETE FROM plat WHERE nomPlat = ? ";
-$req = $bdd->prepare($requete21);
-	   $req->execute(['$nomPlatSuppr']);*/
+
+$requete24 = "DELETE FROM plat WHERE nomPlat = ? ";
+$req = $bdd->prepare($requete24);
+	   $req->execute([$nomPlatSuppr]);
 }
 ?>
+<p><strong><i>Modifier un plat</i></strong></p>
+<p>Vous souhaitez modifier le nom du plat</p>
+<form action="admin.php" method="post">
+    <div>
+        <label for="nomPlatAModif">Nom du plat à modifier :</label>
+        <input type="text" id="nomPlatAModif" name="nomPlatAModif">
+    </div>
+    <br/>
+    <div>
+        <label for="nomPlatModif">Nom du plat modifier :</label>
+        <input type="text" id="nomPlatModif" name="nomPlatModif">
+    </div>
+    <br/>
+    <div>
+    	 <label for="type"></label>
+        <input type="submit" id="okModif" name="okModif">
+    </div>
+<?php
+if (isset ($_POST['okModif'])){
+	$nomPlatModif = $_POST['nomPlatModif'];
+	$nomPlatAModif = $_POST['nomPlatAModif'];
+$requete23 = "UPDATE plat SET nomPlat = ? WHERE nomPlat = ? ";
+$req = $bdd->prepare($requete23);
+	   $req->execute([$nomPlatModif, $nomPlatAModif]);
+}
+
+?>
+<p>Vous souhaitez modifier le prix du plat</p>
 </form>
+<button class="bouton"><a href="index.php">Accueil</a></button>
 </body>
 </html>
